cb74122f1edc069a720a3b36a32d805e885e4f6d
怪兽奥利奥 <90827300+wsh694331170@users.noreply.github.com>
2025-12-22 17:19:51 +0800

    Create main_copy.ts for NestJS application setup

---
+++ src/main_copy.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+    maxAge: 600,
+  });
+  const options = new DocumentBuilder()
+    .setTitle('General-API')
+    .setDescription('general api service')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(8080);
+}
+bootstrap();

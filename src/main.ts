294c7fc4ded27f643bd38fe6195be099b5e24ef1
怪兽奥利奥 <90827300+wsh694331170@users.noreply.github.com>
2025-12-29 15:32:38 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
-  await app.listen(8080);
+  await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
